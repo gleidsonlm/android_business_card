4a5e9bd109256f20a60aed0a74b5ac351a8fabbd
Gleidson Medeiros <gleidsonlm@gmail.com>
2025-08-04 12:36:08 -0300

    fix: updated code version to 1754321445

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.gleidsonlm.businesscard"
         minSdk = 31
         targetSdk = 36
-        versionCode = 1753976536
-        versionName = "August 2, 2025"
+        versionCode = 1754321445
+        versionName = "August 4, 2025"
     }
 
     buildTypes {
